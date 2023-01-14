50698a832ab4101379e9636d47bc6a975507f3b7
enom <satoshi.enom@gmail.com>
2023-01-14 21:51:48 +0900

    first commit

---
+++ ray_tracing_in_one_weekend.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "Satoshi Enomoto"
+description   = "Ray Tracing in One Weekend"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["ray_tracing_in_one_weekend"]
+
+
+# Dependencies
+
+requires "nim >= 1.6.10"
---
+++ src/ray_tracing_in_one_weekend.nim
@@ -0,0 +1,28 @@
+import std/strformat
+
+
+proc main(): cint =
+  const
+    imageWidth = 256
+    imageHeight = 256
+
+  echo "P3"
+  echo fmt"{imageWidth} {imageHeight}"
+  echo "255"
+  for j in 0..<imageHeight:
+    for i in 0..<imageWidth:
+      let
+        r = i.float / (imageWidth - 1)
+        g = j.float / (imageHeight - 1)
+        b = 0.25
+
+        ir = (255.999 * r).int
+        ig = (255.999 * g).int
+        ib = (255.999 * b).int
+
+      echo fmt"{ir} {ig} {ib}"
+  0
+
+
+when isMainModule:
+  quit(main())
